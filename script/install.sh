ca5990cc3829c5ba36263527b4ea6ccd0a10d5fa
AlexisTPS <alexis0boulay@gmail.com>
2020-06-17 17:15:23 +0200

    Fix errors

--- script/install.sh
+++ script/install.sh
@@ -2,6 +2,7 @@
 
 #get the path to this script
 MY_PATH=`dirname "$0"`
+MY_PATH=`( cd "$MY_PATH" && pwd)`
 
 echo "$0: linking Droneswarm to ~/workspace"
 cd ~/workspace/src
--- script/pull_all.sh
+++ script/pull_all.sh
@@ -2,6 +2,7 @@
 
 # get the path to this script
 MY_PATH=`dirname "$0"`
+MY_PATH=`( cd "$MY_PATH" && pwd)`
 
 cd $MY_PATH/..
 
