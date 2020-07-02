cf5dfc80898f5a594dd86265c547ee311489d119
AlexisTPS <alexis0boulay@gmail.com>
2020-07-02 16:04:07 +0200

    fix rename

--- script/install.sh
+++ script/install.sh
@@ -4,9 +4,9 @@
 MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd)`
 
-echo "$0: linking Droneswarm to ~/workspace"
+echo "$0: linking droneswarm_brubotics to ~/workspace"
 cd ~/workspace/src
-ln -sf $MY_PATH/../../Droneswarm
+ln -sf $MY_PATH/../../droneswarm_brubotics
 
 echo "$0: building workspace"
 cd ~/workspace
