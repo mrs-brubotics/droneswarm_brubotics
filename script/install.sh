0a7848e908f26350a97c7483ee1baa9c99fb99a2
AlexisTPS <alexis0boulay@gmail.com>
2020-07-03 11:39:23 +0200

    Simple

--- script/install.sh
+++ script/install.sh
@@ -5,11 +5,11 @@ MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd)`
 
 echo "$0: linking droneswarm_brubotics to ~/workspace"
-cd ~/workspace/src
+cd ~/workspace/src/
 ln -sf $MY_PATH/../../droneswarm_brubotics
 
 echo "$0: install depedencies"
-cd ~/workspace/src/droneswarm_brubotics
+cd droneswarm_brubotics/
 gitman install
 
 echo "$0: modify mrs_workspace"
@@ -17,4 +17,5 @@ cd changed_files/
 ./change_file.sh
 
 echo "$0: building workspace"
+cd ../
 catkin build
