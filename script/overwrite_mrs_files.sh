ed8bb05283572beece33a1aef8fd6d2d63506ec2
AlexisTPS <alexis0boulay@gmail.com>
2020-08-07 11:09:24 +0200

    Add script to overwrite and restore mrs_files

---
+++ script/overwrite_mrs_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# get the path to this script
+MY_PATH=`dirname "$0"`
+MY_PATH=`( cd "$MY_PATH" && pwd)`
+
+#Disable safety features
+cp $MY_PATH/../useful_files/mrs_files/controllers.yaml ~/mrs_workspace/src/uav_core/ros_packages/mrs_uav_managers/config/default
