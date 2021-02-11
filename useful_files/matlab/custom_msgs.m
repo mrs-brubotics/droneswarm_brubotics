a4dd3e02d3562f3922be702954080f3db9a0952a
Bryan Convens <bryan.convens@gmail.com>
2021-02-11 13:20:53 +0100

    updated to work for custom brubotics messages

--- useful_files/matlab/custom_msgs.m
+++ useful_files/matlab/custom_msgs.m
@@ -1,3 +1,10 @@
+%% CTU MRS 
 folderpath = '/home/nuc1/mrs_workspace/src/uav_core/ros_packages'; % change to home/devicename
 rosgenmsg(folderpath) % this might take while
-% follow the 3 steps below
+% run, wait several minutes, and follow the 3 steps below
+
+
+%% Brubotics
+folderpath = '/home/nuc1/workspace/src/droneswarm_brubotics/ros_packages'; % change to home/devicename
+rosgenmsg(folderpath) % this might take while
+% run, wait several minutes, and follow the 3 steps below
