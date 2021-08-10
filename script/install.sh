b079e1d89406cfff4d229796d4f8f9cb17219371
Bryan Convens <bryan.convens@gmail.com>
2021-08-10 11:42:23 +0200

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -4,12 +4,12 @@
 MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd)`
 
-echo "$0: updating all ctu-mrs and mrs-brubotics files"
-./pull_all.sh 
+#echo "$0: updating all ctu-mrs and mrs-brubotics files"
+#./pull_all.sh  # we cannot checkout at ctu's master branches at all times (current bug in their altitude)
 
 echo "$0: building the mrs_workspace"
 cd ~/mrs_workspace
-catkin build
+catkin build # add -j2 for easier compilation
 
 echo "$0: linking = copying droneswarm_brubotics to ~/workspace/src"
 cd ~/workspace/src/
@@ -21,4 +21,4 @@ gitman install
 
 echo "$0: building the workspace"
 cd ../../ #not strictly necessary
-catkin build
+catkin build # add -j2 for easier compilation
