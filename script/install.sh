f774a3478d2418b707575e44fe62e799c85d30d9
Bryan Convens <bryan.convens@gmail.com>
2021-12-15 13:28:18 +0100

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -20,29 +20,28 @@ fi
 # | ------- add command ulimit -n 4096 to .bashrc ------- |
 num=`cat ~/.bashrc | grep "ulimit -n" | wc -l`
 if [ "$num" -lt "1" ]; then
-  echo "Adding source to .bashrc"
+  echo "Adding ulimit to .bashrc"
   # set bashrc
   echo "
 # ulimit shell configuration to increase process file size limit (required for benchmarks):
 command ulimit -n 4096" >> ~/.bashrc
 fi
 
-
 #echo "$0: updating all ctu-mrs and mrs-brubotics files"
 #./pull_all.sh  # we cannot checkout at ctu's master branches at all times (current bug in their altitude)
 
-# echo "$0: building the mrs_workspace"
-# cd ~/mrs_workspace
-# catkin build # add -j2 for easier compilation
+echo "$0: building the mrs_workspace"
+cd ~/mrs_workspace
+catkin build # add -j2 for easier compilation
 
-# echo "$0: linking = copying droneswarm_brubotics to ~/workspace/src"
-# cd ~/workspace/src/
-# ln -sf $MY_PATH/../../droneswarm_brubotics
+echo "$0: linking = copying droneswarm_brubotics to ~/workspace/src"
+cd ~/workspace/src/
+ln -sf $MY_PATH/../../droneswarm_brubotics
 
-# echo "$0: installing depedencies"
-# cd droneswarm_brubotics/
-# gitman install
+echo "$0: installing depedencies"
+cd droneswarm_brubotics/
+gitman install
 
-# echo "$0: building the workspace"
-# cd ../../ #not strictly necessary
-# catkin build # add -j2 for easier compilation
+echo "$0: building the workspace"
+cd ../../ #not strictly necessary
+catkin build # add -j2 for easier compilation
