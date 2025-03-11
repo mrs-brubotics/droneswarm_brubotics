f4843fbc76657fdbc7ec9902bf6a48054e94249f
Bryan Convens <bryan.convens@gmail.com>
2025-03-11 14:34:02 +0100

    Update install_additional_ctu_packages.sh

--- script/install_additional_ctu_packages.sh
+++ script/install_additional_ctu_packages.sh
@@ -16,19 +16,19 @@ cd ~/workspace/src/
 rm -rf trajectory_loader
 git clone https://github.com/ctu-mrs/trajectory_loader 
 cd trajectory_loader
-git checkout 1da108271b2c20f3c16c9e6bc94cd9e807bbdc3c
+# git checkout 1da108271b2c20f3c16c9e6bc94cd9e807bbdc3c
 #catkin build
 echo "$0: mrs_serial"
 cd ~/workspace/src/
 rm -rf mrs_serial
 git clone https://github.com/ctu-mrs/mrs_serial
 cd mrs_serial
-git checkout 8e234320d82f996eb3c1268cc5c232ae1eabdf75
+# git checkout 8e234320d82f996eb3c1268cc5c232ae1eabdf75
 #catkin build
 echo "$0: nimbro_network"
 cd ~/workspace/src/
 rm -rf nimbro_network
 git clone https://github.com/ctu-mrs/nimbro_network
 cd nimbro_network
-git checkout 5068b6413372c309542a95ad522642d9406fa66f
+# git checkout 5068b6413372c309542a95ad522642d9406fa66f
 #catkin build
